83e8cdd4c67a83a040010ccbb42fecb760ca6ac1
calebntende <CalebNtende@my.unt.edu>
2024-03-12 10:14:48 -0400

    pushing something

--- Main/main.cpp
+++ Main/main.cpp
@@ -1,22 +1,46 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
-void OpenFile(fstream &fin, string filename);
+void OpenFile(ifstream &fin, string &filename, int &Timesteps, int &Refreshrate, int &Timecounter, vector<string> &fileplaceholdervalues);
 
-int main() {
+int main()
+{
 
-    fstream fin;
+    ifstream fin;
+    string filename="config1.txt";
+    fin.open(filename);
+    if(fin.fail())
+    {
+        cout << "file not found\n";
+        exit(0);
+    }
 
+     int TimeSteps;
+     int RefreshRate;
+     int TimeCounter;
+     vector<string> fileplaceholder;
+
+    
     cout << "Hello, World!" << endl;
     return 0;
 }
 
-void OpenFile(fstream &fin, string filename) {
-    fin.open(filename, ios::in);
-    if (fin.fail()) {
-        cout << "File not found" << endl;
-        exit(1);
+void OpenFile(ifstream &fin, string &filename, int &Timesteps, int &Refreshrate, int &Timecounter, vector<string> &fileplaceholdervalues)
+{
+    string tempvalues;
+
+    while (!(fin.eof()))
+    {
+        getline(fin, tempvalues, ':');
+        fin >> tempvalues;
+        fileplaceholdervalues.push_back(tempvalues);
     }
+
+    filename = fileplaceholdervalues.at(0);
+    Timesteps = stoi(fileplaceholdervalues.at(1));
+    Refreshrate = stoi(fileplaceholdervalues.at(2));
+    fin.close();
 }
